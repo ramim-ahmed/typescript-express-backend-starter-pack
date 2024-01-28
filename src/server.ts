f70a99d1082cb3d6de9fe2b309a406962379fc30
ramimcodes <ramimahmedraj2@gmail.com>
2024-01-29 00:45:47 +0600

    starter-backend

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,5 @@
-import express from 'express';
-const app = express();
-const port = 3000;
+async function run() {
+  console.log('starter-pack');
+}
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+run();
